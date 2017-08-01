0ccc280b2a8325896a2dc570bf00b25e2dbcf295
dada.mazza <dada.mazza@gmail.com>
2017-08-02 01:39:57 +0300

    spring forgot password controller

--- src/main/java/ua/itea/controller/ForgotPasswordServlet.java
+++ src/main/java/ua/itea/controller/ForgotPasswordServlet.java
@@ -3,24 +3,23 @@ package ua.itea.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("/forgotPassword")
+@Controller
+@RequestMapping("/forgotPassword")
 public class ForgotPasswordServlet extends HttpServlet {
 
     protected Log log = LogFactory.getLog(getClass());
 
-    @Override
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String service() {
 
-        String forwardURL = "/forgotPassword.jsp";
-        log.info("forward : " + forwardURL);
-        request.getRequestDispatcher(forwardURL).forward(request, response);
+        String url = "forgotPassword";
+        log.info("url -> " + url);
+        return url;
     }
 }
